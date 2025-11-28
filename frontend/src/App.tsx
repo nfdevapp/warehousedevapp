d9b4cfd2c5dd9abff9c40c2b34c5b29b31543875
Ivan <>
2025-11-28 11:15:16 +0100

    new project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout.tsx";
+import WarehousePage from "./pages/WarehousePage.tsx";
+import ProductPage from "./pages/ProductPage.tsx";
+import ProductDetailPage from "./pages/ProductDetailPage.tsx";
+
+import './App.css';
+
+function App() {
+    return (
+        <Layout>
+            <Routes>
+                {/* Liste aller Lagerhäuser */}
+                <Route path="/" element={<WarehousePage />} />
+
+                {/* Produkte eines bestimmten Lagerhauses */}
+                <Route path="/warehouse/:warehouseId/products" element={<ProductPage />} />
+
+                {/* Produktdetails */}
+                <Route path="/productdetails/:id" element={<ProductDetailPage />} />
+            </Routes>
+        </Layout>
+    );
+}
+
+export default App;
