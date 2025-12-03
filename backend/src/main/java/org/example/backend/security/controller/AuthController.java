37ecd018715a4265f2e1f1f333cf392106fd59c5
Ivan <>
2025-12-03 14:26:02 +0100

    change folder

--- backend/src/main/java/org/example/backend/security/controller/AuthController.java
+++ backend/src/main/java/org/example/backend/security/controller/AuthController.java
@@ -1,4 +1,4 @@
-package org.example.backend.security;
+package org.example.backend.security.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
