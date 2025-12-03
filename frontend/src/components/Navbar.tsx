fafc8a8a1f997b8d741fab6b69fbc965f71a1dd9
Ivan <>
2025-12-03 14:32:47 +0100

    add logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-// import { useAuth } from "../context/AuthContext"; // Beispiel
+// import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
     const { pathname } = useLocation();
