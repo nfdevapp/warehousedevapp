06230a93d87202a39f3656785e1250332cd0711a
Ivan <>
2025-12-03 15:01:09 +0100

    uncomment for login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,3 +20,35 @@ export default function App() {
         </Routes>
     );
 }
+
+// import { Routes, Route } from "react-router-dom";
+// import Layout from "./components/Layout";
+// import ProtectedRoute from "./components/ProtectedRoute";
+//
+// import LoginPage from "./pages/LoginPage";
+// import WarehousePage from "./pages/WarehousePage";
+// import ProductPage from "./pages/ProductPage";
+//
+// export default function App() {
+//     const user = null; // später durch AuthContext ersetzen
+//
+//     return (
+//         <Routes>
+//             {/* Login zuerst – immer öffentlich */}
+//             <Route path="/login" element={<LoginPage />} />
+//
+//             {/* Geschützte Routen */}
+//             <Route element={<ProtectedRoute user={user} />}>
+//                 <Route element={<Layout />}>
+//                     {/* Hauptseiten */}
+//                     <Route path="/" element={<WarehousePage />} />
+//                     {/*<Route path="/product" element={<ProductPage />} />*/}
+//
+//                     {/* Detailseiten */}
+//                     <Route path="/product/warehouse/:id" element={<ProductPage />} />
+//                 </Route>
+//             </Route>
+//         </Routes>
+//     );
+// }
+
